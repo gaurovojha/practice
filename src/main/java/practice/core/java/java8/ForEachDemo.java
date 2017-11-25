418753221637f3740ea96a83b3ba60092375e26b
ojhag1 <ojhag1@L507B9DEF6478.global.anz.com>
2017-11-25 13:17:18 +0530

    Array rotation finding kth largest element

--- src/main/java/practice/core/java/java8/ForEachDemo.java
+++ src/main/java/practice/core/java/java8/ForEachDemo.java
@@ -7,7 +7,8 @@ import java.util.function.Consumer;
 public class ForEachDemo {
 
 	static List<Integer> values = new ArrayList<Integer>();
-	//Consumer c
+
+	// Consumer c
 	public static void main(String[] args) {
 		values.add(1001);
 		values.add(1002);
@@ -15,14 +16,32 @@ public class ForEachDemo {
 		values.add(1004);
 		values.add(1005);
 		values.add(1006);
-		
-		/*Consumer<Integer> c = i -> System.out.println(i);
-		//values.forEach(i -> System.out.println(i)); //-> is LAMDA
-		values.forEach(c);*/
-		
-		
+
+		/*
+		 * Consumer<Integer> c = i -> System.out.println(i); //values.forEach(i
+		 * -> System.out.println(i)); //-> is LAMDA values.forEach(c);
+		 */
+
 		values.forEach(i -> System.out.println(i));
 		System.out.println(values.stream().findFirst());
+
+		// creating sample Collection
+		List<Integer> myList = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++)
+			myList.add(i);
+		
+		MyConsumer myConsumer = new MyConsumer();
+		myList.forEach(myConsumer);
+
+	}
+}
+
+class MyConsumer implements Consumer<Integer> {
+
+	@Override
+	public void accept(Integer t) {
+		// TODO Auto-generated method stub
+		System.out.println("performing business logic");
 
 	}
 
